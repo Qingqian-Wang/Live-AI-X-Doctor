76002830625dacff83c35861819fa02101bdde1d
Eric Yin <ericyin@EricdeMacBook-Pro-2.local>
2024-03-23 15:23:04 -0400

    add env file

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world():
+    # Replace 'YOUR_ENV_VARIABLE' with the actual name of your environment variable
+    api_key = os.environ.get('GOOGLE_MAPS_API_KEY')
+    print(api_key)
+    return render_template('index.html', api_key=api_key)
 
 
 if __name__ == '__main__':
