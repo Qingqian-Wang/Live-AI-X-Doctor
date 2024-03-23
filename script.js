0e0d9454b9221305bb9e054507720b3c2fedab4f
Raplis Zhang <1195624612@qq.com>
2024-03-23 14:45:09 -0400

    map

---
+++ script.js
@@ -0,0 +1,72 @@
+function initMap() {
+    const defaultLocation = { lat: -34.397, lng: 150.644 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 16,
+        center: defaultLocation,
+    });
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const userLocation = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude,
+                };
+                map.setCenter(userLocation);
+
+                const userMarker = new google.maps.Marker({
+                    position: userLocation,
+                    map: map,
+                    title: "Your Location",
+                    icon: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+                });
+
+                const service = new google.maps.places.PlacesService(map);
+                service.nearbySearch(
+                    {
+                        location: userLocation,
+                        radius: 5000,
+                        type: ["hospital"],
+                    },
+                    (results, status) => {
+                        if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+                            results.forEach((result) => {
+                                createMarker(result, map, userLocation);
+                            });
+                        }
+                    }
+                );
+            },
+            () => {
+                handleLocationError(true, map.getCenter(), map);
+            }
+        );
+    } else {
+        handleLocationError(false, map.getCenter(), map);
+    }
+}
+
+function createMarker(place, map, userLocation) {
+    const marker = new google.maps.Marker({
+        position: place.geometry.location,
+        map: map,
+    });
+
+    const infowindow = new google.maps.InfoWindow({
+        content: `<h3>${place.name}</h3>
+                  <p>${place.vicinity}</p>
+                  <a target="_blank" href="https://www.google.com/maps/dir/?api=1&origin=${userLocation.lat},${userLocation.lng}&destination=${place.geometry.location.lat()},${place.geometry.location.lng()}&travelmode=driving">Guide to here</a>`
+    });
+
+    marker.addListener("click", () => {
+        infowindow.open(map, marker);
+    });
+}
+
+
+function handleLocationError(browserHasGeolocation, pos, map) {
+    alert(browserHasGeolocation ?
+        '地理定位服务失败，请确保您允许了位置访问权限。' :
+        '错误：您的浏览器不支持地理定位。');
+    map.setCenter(pos);
+}
